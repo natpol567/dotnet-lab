0e0a449096e283065eae8225e6b8a1f61764a058
student <student@L22KOMP1790>
2025-03-05 18:13:59 +0100

    my first commit

---
+++ lab1_NP/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Runtime.InteropServices;
+
+namespace lab1_NP
+{
+    internal class Program
+    {
+
+        class FizzBuzz
+        {
+            private int i;
+
+            public FizzBuzz(int i)
+            {
+                this.i = i;
+            }
+
+            public string Check()
+            {
+                if (i % 15 == 0)
+                {
+                    return "FizzBuzz";
+                }
+                else if (i % 3 == 0)
+                {
+                    return "Fizz";
+                }
+                else if (i % 5 == 0)
+                {
+                    return "Buzz";
+                }
+                else
+                    return i.ToString();
+            }
+
+           
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            
+            for (int i = 1; i <= 20; i++)
+            {
+                FizzBuzz fz = new FizzBuzz(i);
+                Console.WriteLine(fz.Check());
+            }  
+
+            
+
+        
+        }
+    }
+}
